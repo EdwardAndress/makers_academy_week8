54d4d050d4d50e6be55860a84469d8bdfd562f45
Edward J. Andress <eddie_andress@hotmail.com>
2014-08-11 11:56:53 +0100

    added restaurant model and list of restaurants on homepage

--- spec/features/restaurants_spec.rb
+++ spec/features/restaurants_spec.rb
@@ -2,12 +2,22 @@ require 'rails_helper'
 
 describe 'restaurants' do 
 
-context 'when the DB is created and no restaurants have been added' do 
-	it 'there should be an appropriate message and option to add restaurants on the home page' do
-		visit '/restaurants'
-		expect(page).to have_content("As yet we have no restaurants listed for this area")
-		expect(page).to have_link("Add a restaurant")
+	context 'when the DB is created and no restaurants have been added' do 
+		it 'there should be an appropriate message and option to add restaurants on the home page' do
+			visit '/restaurants'
+			expect(page).to have_content("As yet we have no restaurants listed for this area")
+			expect(page).to have_link("Add a restaurant")
+		end
 	end
-end
+
+	context 'when the DB contains restaurants' do 
+
+		it 'they should be displayed on the homepage' do
+			Restaurant.create(name: "Vanilla Black")
+			visit ('/restaurants')
+			expect(page).to have_content('Vanilla Black')
+		end
+
+	end
 
 end
