5ac3aedac6e79c5f86448274cb29d42a804785d7
Edward J. Andress <eddie_andress@hotmail.com>
2014-08-12 16:35:00 +0100

    restaurant validations added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ActiveRecord::Base
 	has_many :reviews
 
+	validates :name, uniqueness: true, length: { minimum: 3 }
+
 	def average_rating
 			ratings = self.reviews.map {|review| review.rating}
 			total = ratings.inject {|sum, rating| sum + rating}
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	
+	it 'is valid with a unique name of > 2 characters, starting with a capital letter' do
+	restaurant = Restaurant.new(name: 'The Gate')
+	expect(restaurant).to have(:no).errors_on(:name)
+	end
+
+	it 'is invalid with a non unique name' do
+	Restaurant.create(name: 'The Gate')
+	restaurant = Restaurant.new(name: 'The Gate')
+	expect(restaurant).to have(1).error_on(:name)
+	end
+
+	it 'is invalid with a name < 3 characters' do
+	restaurant = Restaurant.new(name: 'ZK')
+	expect(restaurant).to have(1).error_on(:name)
+	end
+
 end
